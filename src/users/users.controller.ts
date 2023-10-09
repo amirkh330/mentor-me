157e59a0defcfa545b914e1266accc4a98dfe334
Amir Hossein Khakbaz <amir73khakbaz@gmail.com>
2023-10-09 09:53:10 +0430

    create auth file and preparing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,25 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDTO } from './create-user.dto';
 import { UsersService } from './users.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
   constructor(private UsersServices: UsersService) {}
 
   @Get()
+  @UseGuards(AuthGuard())
   findAll() {
-    return this.UsersServices.findAll();
+    return this.UsersServices.findAll()
   }
 
   @Get(':id')
   findById(@Param('id') id) {
-    return this.UsersServices.findById(id);
+    return this.UsersServices.findById(id)
   }
 
   @Post()
   createUSer(@Body() CreateUserDTO: CreateUserDTO) {
-    return this.UsersServices.createUser(CreateUserDTO);
+    return this.UsersServices.createUser(CreateUserDTO)
   }
 }
